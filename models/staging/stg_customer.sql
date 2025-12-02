ce3c17db990a3f0c7b5723ebf3d8c860cd46c72f
Khổng Đức Quang <113845416+KhongDucQuang@users.noreply.github.com>
2025-12-02 16:52:49 +0700

    Update stg_customer.sql

--- models/staging/stg_customer.sql
+++ models/staging/stg_customer.sql
@@ -1,10 +1,16 @@
+{{ config(
+    materialized='table',
+    catalog='hive_catalog',
+    schema='gold',
+    properties={
+      'file_format': 'parquet',
+      'compression': 'snappy'
+    }
+) }}
+
 SELECT
-    id, 
-    name,
-    email,
-    created_at,
-    partition_date,
-    -- Giả sử bạn muốn thêm cột thời gian xử lý
+    pk_branch_bank,
+    fk_bank_code,
     NOW() as processed_at
 FROM
-    {{ source('mbs_bronze', 'customer_raw') }}
+    {{ source('list_silver_tables', 'dim_t_list_branch_bank_adv_wdr') }}
